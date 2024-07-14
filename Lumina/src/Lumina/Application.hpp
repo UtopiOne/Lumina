d8322118094cf8f45c61b0cc7709b36c4321e41e
utopione <arseniyver@gmail.com>
2024-07-15 02:08:25 +0300

    Shaders

--- Lumina/src/Lumina/Application.hpp
+++ Lumina/src/Lumina/Application.hpp
@@ -5,6 +5,7 @@
 #include "Lumina/Events/ApplicationEvent.hpp"
 #include "Lumina/Layer.hpp"
 #include "Lumina/LayerStack.hpp"
+#include "Lumina/Renderer/Shader.hpp"
 #include "Window.hpp"
 #include "ImGui/ImGuiLayer.hpp"
 
@@ -38,6 +39,8 @@ private:
     LayerStack m_LayerStack;
 
     unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+    std::unique_ptr<Shader> m_Shader;
+
 
 private:
     static Application* s_Instance;
